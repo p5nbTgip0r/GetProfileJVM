b74e821839cad4713a418aad8c55c0e2818519b6
p5nbTgip0r <cb3f02c5@opayq.com>
2020-06-01 23:18:34 -0700

    Create parser object for providing singleton access to Jackson

---
+++ src/main/java/cc/insulin/getprofile/Parser.kt
@@ -0,0 +1,16 @@
+package cc.insulin.getprofile
+
+import cc.insulin.getprofile.nightscout.data.ProfileChange
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+
+object Parser {
+    val objectMapper = ObjectMapper().registerModule(KotlinModule())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    fun parseProfileChanges(input: String): List<ProfileChange>? {
+        return objectMapper.readValue(input)
+    }
+}
