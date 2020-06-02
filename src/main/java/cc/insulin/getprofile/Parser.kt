5020e446f263942079af2003ae298ab260e656a5
p5nbTgip0r <cb3f02c5@opayq.com>
2020-06-01 23:43:51 -0700

    Specify Jackson variable type explicitly

--- src/main/java/cc/insulin/getprofile/Parser.kt
+++ src/main/java/cc/insulin/getprofile/Parser.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 
 object Parser {
-    val objectMapper = ObjectMapper().registerModule(KotlinModule())
+    val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     fun parseProfileChanges(input: String): List<ProfileChange>? {
