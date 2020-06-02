83015efde528d0c06b19512564997af5f5983419
p5nbTgip0r <cb3f02c5@opayq.com>
2020-06-01 23:24:10 -0700

    Create method in `Nightscout` class for getting an instance of `HttpUrl` for profiles

--- src/main/java/cc/insulin/getprofile/nightscout/data/Nightscout.kt
+++ src/main/java/cc/insulin/getprofile/nightscout/data/Nightscout.kt
@@ -1,4 +1,14 @@
 package cc.insulin.getprofile.nightscout.data
 
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+
 data class Nightscout(val url: String,
-                      val authentication: String? = null)
+                      val authentication: String? = null) {
+
+    fun getProfileURL(): HttpUrl {
+        return url.toHttpUrl().newBuilder()
+                .addPathSegments("api/v1/profile.json")
+                .build()
+    }
+}
