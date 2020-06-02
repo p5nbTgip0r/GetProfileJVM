25a987588da9f299d4f81d135b043e8b9a359d40
p5nbTgip0r <cb3f02c5@opayq.com>
2020-06-01 23:33:54 -0700

    Create converters for NS profiles to OAPS profiles. This needs to be improved upon, but for right now it should work fine for autotune

--- src/main/java/cc/insulin/getprofile/oaps/OAPSConverter.kt
+++ src/main/java/cc/insulin/getprofile/oaps/OAPSConverter.kt
@@ -1,15 +1,22 @@
 package cc.insulin.getprofile.oaps
 
+import cc.insulin.getprofile.data.GlucoseUnits
+import cc.insulin.getprofile.nightscout.data.NSProfile
 import cc.insulin.getprofile.nightscout.data.profile.ScheduleEntry
+import cc.insulin.getprofile.oaps.data.OAPSProfile
 import cc.insulin.getprofile.oaps.data.profile.BasalEntry
+import cc.insulin.getprofile.oaps.data.profile.ISFProfile
+import cc.insulin.getprofile.oaps.data.profile.SensitivityEntry
+import org.apache.logging.log4j.kotlin.Logging
 
-object OAPSConverter {
+object OAPSConverter : Logging {
+
     fun convertBasal(scheduleEntry: ScheduleEntry): BasalEntry {
         val rate = scheduleEntry.value.toDouble()
         val minutes = scheduleEntry.timeAsSeconds / 60
 
         val entry = BasalEntry(minutes, rate)
-        println(entry)
+        logger.debug("Converted basal entry: $scheduleEntry -> $entry")
         return entry
     }
 
@@ -20,7 +27,36 @@ object OAPSConverter {
             basalEntries.add(convertBasal(entry))
         }
 
-        println(basalEntries)
+        logger.debug("Basal entries: $basalEntries")
         return basalEntries
+    }
+
+    // todo: allow not converting units
+    // todo: support multiple sensitivity times
+    fun convertSensitivity(entries: List<ScheduleEntry>, units: GlucoseUnits): List<SensitivityEntry> {
+        val sensitivityEntries = mutableListOf<SensitivityEntry>()
+        val nsEntry = entries[0]
+        var sens = nsEntry.value.toDouble()
+        if (units == GlucoseUnits.MMOL) {
+            // autotune requires the profile to be in mg/dl, so we must convert the units
+            sens *= 18
+        }
+        val newEntry = SensitivityEntry(0, sens, 0, 0, 1440)
+        sensitivityEntries.add(newEntry)
+
+        logger.debug("ISF entries: $sensitivityEntries")
+        return sensitivityEntries
+    }
+
+    fun convertProfile(nsProfile: NSProfile): OAPSProfile {
+        val basal = convertBasals(nsProfile.basal)
+        val sens = convertSensitivity(nsProfile.sens, nsProfile.units)
+        val isfProfile = ISFProfile(sens)
+        // todo: support carb schedules
+        val carbRatio = nsProfile.carbRatio[0].value.toDouble()
+
+        val oapsProfile = OAPSProfile(8.0, nsProfile.dia.toDouble(), basal, isfProfile, carbRatio)
+        logger.debug("OpenAPS-converted profile: $oapsProfile")
+        return oapsProfile
     }
 }
